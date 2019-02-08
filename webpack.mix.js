e7f764ee7c68383390f0036fe0f0d64f84451aac
CRYDEsigN <Cryden_da@mail.ru>
2019-02-08 16:08:23 +0300

    add testbook

--- webpack.mix.js
+++ webpack.mix.js
@@ -7,7 +7,7 @@ mix
     .js('./source/js/main.js', 'js')
     .sass('./source/sass/styles.sass', 'css')
     .options({
-        processCssUrls: true
+        processCssUrls: false
     })
 
     .browserSync({
